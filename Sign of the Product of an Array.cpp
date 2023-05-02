3152a350891bf364557aa3672a444e7c5097c1ec
WajeehaCodes <130116477+WajeehaCodes@users.noreply.github.com>
2023-05-02 16:14:41 +0500

    Create Sign of the Product of an Array.cpp

---
+++ Sign of the Product of an Array.cpp
@@ -0,0 +1,15 @@
+int arraySign(vector<int>& nums) {
+    int product = 1;
+    for(int i=0; i<nums.size(); i++) {
+        if(nums[i] == 0)
+            return 0;
+        if(nums[i] > 0)
+            product *= 1;
+        else if(nums[i] < 0)
+            product *= -1;
+    }
+    if(product > 0)
+        return 1;
+    else 
+        return -1;
+}
